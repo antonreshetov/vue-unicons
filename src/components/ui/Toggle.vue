8ab77e46e265922c8cce7077989e78c155859c18
Anton Reshetov <reshetov.art@gmail.com>
2019-02-07 11:48:54 +0300

    chore(ui): add toggle

---
+++ src/components/ui/Toggle.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    class="toggle"
+    :class="{'toggle--active': value}"
+    @click="toggle"
+  />
+</template>
+
+<script>
+export default {
+  name: '',
+
+  model: {
+    prop: 'value',
+    event: 'click'
+  },
+
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    toggle () {
+      this.$emit('click', !this.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../../assets/scss/vars";
+
+.toggle {
+  display: inline-block;
+  border: 1px solid $color-border;
+  height: 20px;
+  width: 40px;
+  line-height: 20px;
+  position: relative;
+  border-radius: 100px;
+  cursor: pointer;
+  &::after {
+    content: "";
+    position: absolute;
+    width: 16px;
+    height: 16px;
+    border-radius: 100%;
+    background-color: royalblue;
+    top: 1px;
+    left: 1px;
+    transition: all .2s;
+  }
+  &--active {
+    &::after {
+      left: 100%;
+      margin-left: -17px;
+    }
+  }
+}
+</style>
