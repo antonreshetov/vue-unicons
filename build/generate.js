18c940f5ca527950bb52424bed087b9462bd9495
Anton Reshetov <reshetov.art@gmail.com>
2019-02-12 15:52:00 +0300

    refactor: icons generate script

--- build/generate.js
+++ build/generate.js
@@ -4,37 +4,52 @@ const unicons = require('unicons/icons.json')
 const Handlebars = require('handlebars')
 const camelCase = require('lodash.camelcase')
 const dist = path.resolve(__dirname, '../src/')
+const util = require('util')
+const readFile = util.promisify(fs.readFile)
+const chalk = require('chalk')
 
-function build () {
-  const iconTemplate = fs.readFileSync(path.resolve(__dirname, './icon.hbs'), 'utf-8', err => {
-    if (err) console.error(err)
+function generate () {
+  const promises = unicons.map(icon => {
+    return readFile(
+      path.resolve('node_modules/unicons', icon.svg),
+      'utf-8'
+    ).then(svg => {
+      // add formatted name
+      const name = camelCase(icon.name)
+      icon.nameFormatted = 'uni' + name.charAt(0).toUpperCase() + name.slice(1)
+      // trim spaces, tabs, new lines from svg
+      svg = svg.replace(/\r+|\n+|\t+/gm, '')
+      // get svg path
+      const svgPath = svg.match(/<path.*\/>/g)
+      // add svg path to icon
+      if (svgPath) icon.path = svgPath[0]
+
+      return icon
+    })
   })
-
-  const icons = unicons.map(icon => {
-    let file = fs.readFileSync(path.resolve('node_modules/unicons', icon.svg), 'utf-8', err => {
+  // await promises
+  Promise.all(promises).then(icons => {
+    // get icon template
+    const iconTemplate = fs.readFileSync(
+      path.resolve(__dirname, './icon.hbs'),
+      'utf-8',
+      err => {
+        if (err) console.error(err)
+      }
+    )
+    // compile icon list
+    const template = Handlebars.compile(iconTemplate)
+    const iconList = template({ icons })
+    // Divided by icon list
+    fs.writeFile(`${dist}/icons.js`, iconList, err => {
       if (err) console.error(err)
     })
-
-    // trim spaces, tabs, new lines
-    file = file.replace(/\r+|\n+|\t+/gm, '')
-
-    const svgPath = file.match(/<path.*\/>/g)
-
-    if (svgPath) icon.path = svgPath[0]
-
-    const name = camelCase(icon.name)
-    icon.nameFormatted = 'uni' + name.charAt(0).toUpperCase() + name.slice(1)
-
-    return icon
+    // Icons list to demo app
+    fs.writeFile(`${dist}/demo.json`, JSON.stringify(icons), err => {
+      if (err) console.error(err)
+    })
+    console.log(chalk.green('All done'))
   })
-
-  const template = Handlebars.compile(iconTemplate)
-  const file = template({ icons })
-
-  // Divided by icon list
-  fs.writeFileSync(`${dist}/icons.js`, file)
-  // Icons list to demo app
-  fs.writeFileSync(`${dist}/demo.json`, JSON.stringify(icons))
 }
 
-build()
+generate()
