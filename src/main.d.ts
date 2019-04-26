19b2bb6185e70a74dfea6ce70b4d56d803fe36a9
George Pickering <29524044+tedjenkins@users.noreply.github.com>
2019-04-26 10:58:31 +0100

    types: add types declaration file
    close #14
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	new file:   package-lock.json
    #	modified:   package.json
    #	new file:   src/main.d.ts
    #

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,6 +1,10 @@
-declare module 'vue-unicons' {
-  export function install(Vue: any): void
+import Vue, { VueConstructor } from 'vue'
+
+declare module "vue-unicons" {
+  export function install(Vue: VueConstructor, options: object): void
   export function add(icons: string[]): void
 }
 
-declare module 'vue-unicons/src/icons'
+declare module "vue-unicons/src/icons" {
+  export const icons: { name: string; path: string }
+}
