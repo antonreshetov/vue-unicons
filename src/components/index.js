cbdb51679b3e71ed343bc7836c4e11b9d31090fa
Mustafa Omar <46113191+MustafaOmarIbrahim@users.noreply.github.com>
2020-02-04 05:41:06 +0200

    Add the ability to set Unicon options globally.
    Add the ability to customize the Unicon component options globally.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,18 @@
 import Unicon from './Unicon.vue'
 
 export default {
-  install (Vue, options) {
+  install (Vue, options = {}) {
+
+    const props = Unicon.props
+
+    for (var option in options) {
+      if (props.hasOwnProperty(option)) {
+        Unicon.props[option] = {
+          default: options[option]
+        }
+      }
+    }
+
     Vue.component(Unicon.name, Unicon)
   },
   add (icons) {
