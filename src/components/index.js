644fc006995d72c2fb3794914bed4309b72afe20
Anton Reshetov <reshetov.art@gmail.com>
2020-07-05 08:52:55 +0300

    feat: add global config

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,14 @@
 import Unicon from './Unicon.vue'
 
 export default {
-  install (Vue, options) {
+  install (Vue, options = {}) {
     Vue.component(Unicon.name, Unicon)
+
+    for (const [ k, v ] of Object.entries(options)) {
+      if (Unicon.props[k]) {
+        Unicon.props[k].default = v
+      }
+    }
   },
   add (icons) {
     Unicon.add(icons)
